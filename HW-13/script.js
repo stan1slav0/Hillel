1ad77233a1ea3de6d175a27d6542df615d7ad024
Stanislav <pingo.mw2@gmail.com>
2022-09-25 20:27:26 +0300

    first init

---
+++ HW-13/script.js
@@ -0,0 +1,28 @@
+class Burger {
+  constructor(name, size) {
+    this.name = name
+    this.size = size
+    this.ingredients = [`cutlet`, `salada`, `tomato`]
+  }
+
+  setAdditionalIngredients(...args) {
+    args.map(elem => this.ingredients.push(elem))
+    return this
+  }
+}
+
+class Cheeseburger extends Burger {
+  constructor(name, size) {
+    super(name, size)
+    this.ingredients.push(`cheese`)
+  }
+}
+
+let Hamburger = new Burger('Hamburger', 'small')
+Hamburger.setAdditionalIngredients(`egg`, `onion`)
+
+let myCheeseburger = new Cheeseburger('Cheeseburger', 'small')
+myCheeseburger.setAdditionalIngredients(`egg`, `onion`)
+
+console.log(Hamburger)
+console.log(myCheeseburger)
